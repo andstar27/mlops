c0eed9206e5f7ae059a3a6ca0da8c2933670aefa
andstar27 <148645595+andstar27@users.noreply.github.com>
2024-06-16 19:23:13 +0300

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,10 +8,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "mlstudy-test12345"
-    key = "tfstate/terraform.tfstate"
+    bucket         = "mlstudy-test12345"
+    key            = "tfstate/terraform.tfstate"
     dynamodb_table = "mlopstab"
-    region = "eu-west-1"
+    region         = "eu-west-1"
   }
 }
 provider "aws" {
