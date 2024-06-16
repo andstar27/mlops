6e94b295a69d38b7f4f794eb1c87a47ce81a0021
andstar27 <148645595+andstar27@users.noreply.github.com>
2024-06-16 19:50:12 +0300

    Update app_runner.tf

--- infra/app_runner.tf
+++ infra/app_runner.tf
@@ -6,16 +6,19 @@ resource "aws_apprunner_service" "chatbot" {
   service_name = "chatbot"
   source_configuration {
     image_repository {
-      image_identifier      = "211125601087.dkr.ecr.us-east-1.amazonaws.com/test-chatbot:tag"
-      image_repository_type = "ECR"
       image_configuration {
         port = "8080"
         runtime_environment_variables = {
           HUGGINGFACEHUB_API_TOKEN = local.hf_token
         }
       }
+      image_identifier      = "211125601087.dkr.ecr.us-east-1.amazonaws.com/test-chatbot:tag"
+      image_repository_type = "ECR"
     }
-    instance_configuration {
+auto_deployments_enabled = false
+}
+
+instance_configuration {
       cpu    = "1024"
       memory = "2048"
     }
@@ -25,8 +28,3 @@ resource "aws_apprunner_service" "chatbot" {
       }
     }
   }
-  tags = {
-    Name        = "Chatbot"
-    Environment = "Production"
-  }
-}
